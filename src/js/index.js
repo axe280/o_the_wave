8e0c1899697d26256c763e5a61bc1028a9b07f6f
axe280 <axeworkmail@gmail.com>
2021-02-09 17:03:46 +0200

    cart products and order form

--- src/js/index.js
+++ src/js/index.js
@@ -154,6 +154,23 @@ $(function () {
     $(this).addClass('active').siblings('button').removeClass('active')
   })
 
+  // plus/minus value
+  $('.minus-input').click(function () {
+    var $input = $(this).parent().find('input')
+    var count = parseInt($input.val()) - 1
+    count = count < 1 ? 1 : count
+    $input.val(count)
+    $input.change()
+    return false
+  })
+
+  $('.plus-input').click(function () {
+    var $input = $(this).parent().find('input')
+    $input.val(parseInt($input.val()) + 1)
+    $input.change()
+    return false
+  })
+
   // // data-lity
   // $(document).on('lity:ready', function (e, instance) {
   //   addDocumentScrollBlocker()
